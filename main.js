d9f44f90051966865b6c9ee40b25534716d85885
code express <codeexpress333@gmail.com>
2020-09-28 10:45:56 +0530

    animation text anime.js

---
+++ main.js
@@ -0,0 +1,21 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.text');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.text .letter',
+        scale: [4, 1],
+        opacity: [0, 1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: (el, i) => 70 * i
+    }).add({
+        targets: '.text',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
+
